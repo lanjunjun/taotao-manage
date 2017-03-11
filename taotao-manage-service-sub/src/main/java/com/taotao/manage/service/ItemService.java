0902d0d54967bb97cfa31145a37c2c378801860a
lanjunjun <2309936649@qq.com>
2017-03-12 00:33:47 +0800

    查询商品信息

--- taotao-manage-service-sub/src/main/java/com/taotao/manage/service/ItemService.java
+++ taotao-manage-service-sub/src/main/java/com/taotao/manage/service/ItemService.java
@@ -1,30 +1,61 @@
 package com.taotao.manage.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.abel533.entity.Example;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.manage.mapper.ItemMapper;
 import com.taotao.manage.pojo.Item;
 import com.taotao.manage.pojo.ItemDesc;
+
 @Service
-public class ItemService extends BaseService<Item>{
+public class ItemService extends BaseService<Item> {
 
 	@Autowired
 	ItemDescService itemDescService;
+	
+	@Autowired
+	private ItemMapper itemMapper;
+
 	/**
 	 * 保存商品数据
+	 * 
 	 * @param item
 	 */
-	public void saveItem(Item item,String desc) {
-		//保存商品
+	public void saveItem(Item item, String desc) {
+		// 保存商品
 		item.setId(null);
 		item.setStatus(1);
 		super.save(item);
-		
-		//保存商品描述
+
+		// 保存商品描述
 		ItemDesc itemDesc = new ItemDesc();
 		itemDesc.setItemId(item.getId());
 		itemDesc.setItemDesc(desc);
 		this.itemDescService.save(itemDesc);
+
+	}
+
+	/**
+	 * 根据条件分页查询商品
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	public PageInfo<Item> queryItemListByPage(Integer page, Integer rows) {
+		PageHelper.startPage(page, rows);
+		//声明查询条件
+		Example example = new Example(Item.class);
+		//设置排序
+		example.setOrderByClause("updated DESC");
+		example.createCriteria().andEqualTo("status", 1);
+		List<Item> list = this.itemMapper.selectByExample(example);
+		PageInfo<Item> pageInfo = new PageInfo<Item>(list);
+		return pageInfo;
 	}
 
 }
--- taotao-manage-web-sub/src/main/java/com/taotao/manage/web/controller/ItemController.java
+++ taotao-manage-web-sub/src/main/java/com/taotao/manage/web/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.taotao.manage.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,24 +10,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.github.pagehelper.PageInfo;
 import com.taotao.manage.pojo.Item;
 import com.taotao.manage.service.ItemService;
+import com.taotao.manage.web.bean.EasyUIResult;
 
 @Controller
 @RequestMapping("item")
 public class ItemController {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
 	ItemService itemService;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> saveItem(Item item,@RequestParam("desc")String desc){
+	public ResponseEntity<Void> saveItem(Item item,
+			@RequestParam("desc") String desc) {
 		try {
-			this.itemService.saveItem(item,desc);
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("新增商品，传入的参数是：item = {},desc = {}", item, desc);
+			}
+			this.itemService.saveItem(item, desc);
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("新增商品成功！itemId = {}", item.getId());
+			}
 			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} catch (Exception e) {
+			if (LOGGER.isErrorEnabled()) {
+				LOGGER.error("新增商品失败，新增商品的信息是：item=" + item.toString(), e);
+			}
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	}
+	
+	/**
+	 * 分页查询商品数据
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<EasyUIResult> queryItemListByPage(@RequestParam(value = "page",defaultValue = "1")Integer page,@RequestParam(value = "rows",defaultValue = "30")Integer rows){
+		
+		try {
+			PageInfo<Item> pageInfo = this.itemService.queryItemListByPage(page,rows);
+			EasyUIResult easyUIResult = new EasyUIResult(pageInfo.getTotal(), pageInfo.getList());
+			return ResponseEntity.ok().body(easyUIResult);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 	}
 }
